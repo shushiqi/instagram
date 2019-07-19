30ef78c7b64a1a39485712e91808b11e7833358d
shushiqi <1539588925@qq.com>
2019-07-19 21:29:21 +0800

    添加登录

--- service/app/controller/login.js
+++ service/app/controller/login.js
@@ -3,6 +3,28 @@
 const Controller = require('egg').Controller;
 
 class LoginController extends Controller {
+  async loginIn() {
+    const { ctx } = this
+    const { password, email } = ctx.request.body
+
+    const token = await ctx.service.user.login(password, email)
+    if(token){
+      const opts = {
+        path: '/',
+        maxAge: 1000*60*60*24*7,
+        httpOnly: false,
+        domaim: '127.0.0.1'
+      }
+      ctx.cookies.set(this.config.auth_cookie_name, token, opts)
+      ctx.status = 200
+      ctx.body= {
+        msg: '登录成功'
+      }
+    }else{
+      ctx.throw(400, '邮箱或密码错误')
+    }
+  
+  }
   async register() {
     const { ctx } = this;
     const { username, password, email } = ctx.request.body
--- service/app/service/user.js
+++ service/app/service/user.js
@@ -1,12 +1,34 @@
 const Service = require('egg').Service
 const uuid = require('uuid')
+const crypto = require('crypto')
+const jwt = require('jsonwebtoken')
+
 class UserService extends Service {
   async register(user) {
-    const { ctx } = this
-    user.userId = uuid.v4().replace(/-/g, '')
-    console.log('1111',user.userId)
+    const { ctx, app } = this
+    user.user_id = uuid.v4().replace(/-/g, '')
+    // console.log('1111',user.usee_id)
     const queryResult = await this.hasRegister(user.email)
     console.log(queryResult)
+    if (queryResult) {
+      ctx.status = 200,
+        ctx.body = {
+          msg: '邮箱已被使用',
+          flag: false
+        }
+      return
+    }
+    // 加密保存
+    user.password = crypto.createHmac('sha256', app.config.password_secret).update(user.password).digest('hex')
+
+    const userInfo = await this.ctx.model.User.create(user)
+    ctx.status = 200,
+      ctx.body = {
+        msg: '注册成功',
+        user: user.user_id,
+        flag: true
+      }
+    return userInfo.dataValues
   }
 
   async hasRegister(email) {
@@ -15,11 +37,34 @@ class UserService extends Service {
         email: email
       }
     })
-    if(user && user.dataValues.userId){
+    if (user && user.dataValues.user_id) {
       return true
     }
     return false
     // console.log('---', this.ctx.model.User)
+  }
+
+  async login(password, email) {
+    const { app } = this
+    const existUser = await this.getUserByMail(email)
+    if (!existUser) {
+      return null
+    }
+    const passhash = existUser.password
+    const equal = passhash == crypto.createHmac('sha256', app.config.password_secret).update(password).digest('hex')
+    if (!equal) {
+      return false
+    }
+    const token = jwt.sign({ user_id: existUser.user_id }, app.config.jwtSecret, { expiresIn: '7d' })
+    return token
+  }
+
+  async getUserByMail(email) {
+    return this.ctx.model.User.findOne({
+      where: {
+        email
+      }
+    })
   }
 }
 
